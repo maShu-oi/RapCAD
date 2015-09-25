maShu-oi/RapCAD

commit 9649f79d0f0678fbfdc910ea95f70b5233989386

Make output precision is configurable in prefernces.

src/point.cpp
@@ -53,23 +53,18 @@ void Point::getXY(decimal& x, decimal& y) const
 
 QString Point::toString() const
 {
-	return toString(16,true);
+	return toString(false);
 }
 
-QString Point::toString(int precision) const
-{
-	return toString(precision,false);
-}
-
-QString Point::toString(int precision,bool trim) const
+QString Point::toString(bool trim) const
 {
 	QString res;
 	res.append("[");
-	res.append(to_string(x,precision,trim));
+	res.append(to_string(x,trim));
 	res.append(",");
-	res.append(to_string(y,precision,trim));
+	res.append(to_string(y,trim));
 	res.append(",");
-	res.append(to_string(z,precision,trim));
+	res.append(to_string(z,trim));
 	res.append("]");
 
 	return res;

rapcad.pro
@@ -303,8 +303,7 @@ SOURCES += \
 	src/module/volumesmodule.cpp \
 	src/node/volumesnode.cpp \
 	src/cgalvolume.cpp \
-	src/function/numfunction.cpp \
-	src/node/auxilarynode.cpp
+	src/function/numfunction.cpp
 
 HEADERS  += \
 	src/mainwindow.h \
@@ -517,8 +516,7 @@ HEADERS  += \
 	src/module/volumesmodule.h \
 	src/node/volumesnode.h \
 	src/cgalvolume.h \
-	src/function/numfunction.h \
-	src/node/auxilarynode.h
+	src/function/numfunction.h
 
 FORMS += \
 	src/mainwindow.ui \

src/cgalexport.cpp
@@ -94,10 +94,6 @@ void CGALExport::exportAsciiSTL(QString filename)
 		return;
 	}
 	QTextStream output(&data);
-	//These settings are more conservative and hopefully offer
-	//better compatability
-	output.setRealNumberPrecision(6);
-	output.setRealNumberNotation(QTextStream::ScientificNotation);
 
 	output << "solid RapCAD_Model\n";
 
@@ -122,18 +118,18 @@ void CGALExport::exportAsciiSTL(QString filename)
 			CGAL::FT l=inexact_sqrt(ls);
 			CGAL::Vector3 un=n/l;
 
-			decimal x1 = to_decimal(p1.x());
-			decimal y1 = to_decimal(p1.y());
-			decimal z1 = to_decimal(p1.z());
-			decimal x2 = to_decimal(p2.x());
-			decimal y2 = to_decimal(p2.y());
-			decimal z2 = to_decimal(p2.z());
-			decimal x3 = to_decimal(p3.x());
-			decimal y3 = to_decimal(p3.y());
-			decimal z3 = to_decimal(p3.z());
-			decimal nx=to_decimal(un.x());
-			decimal ny=to_decimal(un.y());
-			decimal nz=to_decimal(un.z());
+			QString x1=to_string(p1.x());
+			QString y1=to_string(p1.y());
+			QString z1=to_string(p1.z());
+			QString x2=to_string(p2.x());
+			QString y2=to_string(p2.y());
+			QString z2=to_string(p2.z());
+			QString x3=to_string(p3.x());
+			QString y3=to_string(p3.y());
+			QString z3=to_string(p3.z());
+			QString nx=to_string(un.x());
+			QString ny=to_string(un.y());
+			QString nz=to_string(un.z());
 
 			output << "  facet normal " << nx << " " << ny << " " << nz << "\n";
 			output << "    outer loop\n";

src/decimal.cpp
@@ -17,6 +17,7 @@
  */
 
 #include "decimal.h"
+#include "preferences.h"
 
 static inline int amountToChop(QString s)
 {
@@ -38,18 +39,14 @@ static inline int amountToChop(QString s)
 
 QString to_string(const decimal d)
 {
-	return to_string(d,16,true);
+	return to_string(d,true);
 }
 
-QString to_string(const decimal d, const int precision)
+QString to_string(const decimal d,const bool trim)
 {
-	return to_string(d,precision,false);
-}
-
-QString to_string(const decimal d,const int precision,const bool trim)
-{
+	Preferences* p=Preferences::getInstance();
 	QString res;
-	res.setNum(d,'f',precision);
+	res.setNum(d,'f',p->getPrecision());
 
 	if(trim) {
 		//Trim trailing zeros.

src/decimal.h
@@ -25,8 +25,7 @@
 typedef double decimal;
 
 QString to_string(const decimal);
-QString to_string(const decimal,const int);
-QString to_string(const decimal,const int,const bool);
+QString to_string(const decimal,const bool);
 
 #if USE_CGAL
 
@@ -50,13 +49,13 @@ inline decimal to_decimal(NT n)
 template<class NT>
 inline decimal inexact_sqrt(NT const& n)
 {
-	return sqrt(to_decimal(n));
+	return sqrt(to_double(n));
 }
 
 template<class NT>
 inline QString to_string(NT const& n)
 {
-	return to_string(to_decimal(n));
+	return to_string(to_double(n));
 }
 
 #endif
@@ -65,7 +64,6 @@ inline decimal to_decimal(QString s)
 {
 	return s.toDouble();
 }
-
 
 inline decimal to_decimal(QString s,bool* ok)
 {

src/module/boundsmodule.cpp
@@ -23,17 +23,11 @@
 BoundsModule::BoundsModule() : Module("bound")
 {
 	auxilary=true;
-	addParameter("precision");
 }
 
 Node* BoundsModule::evaluate(Context* ctx)
 {
-	NumberValue* precVal=dynamic_cast<NumberValue*>(getParameterArgument(ctx,0));
-
 	BoundsNode* n=new BoundsNode();
-	if(precVal)
-		n->setPrecision(precVal->getNumber());
-
 	n->setChildren(ctx->getInputNodes());
 	return n;
 }

src/module/radialsmodule.cpp
@@ -24,17 +24,11 @@
 RadialsModule::RadialsModule() : Module("radial")
 {
 	auxilary=true;
-	addParameter("precision");
 }
 
 Node* RadialsModule::evaluate(Context* ctx)
 {
-	NumberValue* precVal=dynamic_cast<NumberValue*>(getParameterArgument(ctx,0));
-
 	RadialsNode* n=new RadialsNode();
-	if(precVal)
-		n->setPrecision(precVal->getNumber());
-
 	n->setChildren(ctx->getInputNodes());
 	return n;
 }

src/module/volumesmodule.cpp
@@ -8,7 +8,6 @@ VolumesModule::VolumesModule() : Module("volume")
 {
 	auxilary=true;
 	addParameter("mass");
-	addParameter("precision");
 }
 
 Node* VolumesModule::evaluate(Context* ctx)
@@ -18,12 +17,7 @@ Node* VolumesModule::evaluate(Context* ctx)
 	if(massVal)
 		mass=massVal->isTrue();
 
-	NumberValue* precVal=dynamic_cast<NumberValue*>(getParameterArgument(ctx,1));
-
 	VolumesNode* n=new VolumesNode();
-	if(precVal)
-		n->setPrecision(precVal->getNumber());
-
 	n->setCalcMass(mass);
 	n->setChildren(ctx->getInputNodes());
 	return n;

commit 7fcd095d24edfc9248a3a00d5db4c219ce1dd5fb

Correct trimming of zeros in output.
Fix to bug introduced in 9649f79d0f0678fbfdc910ea95f70b5233989386.

src/point.cpp
@@ -53,7 +53,7 @@ void Point::getXY(decimal& x, decimal& y) const
 
 QString Point::toString() const
 {
-	return toString(false);
+	return toString(true);
 }
 
 QString Point::toString(bool trim) const